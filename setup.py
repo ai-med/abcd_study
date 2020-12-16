8694008350cf40ab8a68b60eeacf914872a7474a
richardgaus <richardgaus@outlook.de>
2020-12-16 19:41:00 +0100

    Create repo from local folder

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Explore the predictability of various psychiatric diagnoses based on neuroimaging features',
+    author='Richard Gaus',
+    license='MIT',
+)
